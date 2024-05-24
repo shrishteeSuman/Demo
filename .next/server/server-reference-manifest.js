c2e86a8e5ab5a3b95d7b35fab2caed1604158956
ssuman98 <ssuman@linius.com>
2024-05-24 22:08:41 +0530

    Space added

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"0pF2cWhLzjue5Ersfg49T9C1RRMcSKcmlimzEUbMCCY=\"\n}"
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function About(props:any) {
     return (
-        <div className='text-center text-5xl m-20'>ABOUT PAGE</div>
+        <div className='text-center text-5xl m-20'>ABOUT PAGE  </div>
     );
 }
 
